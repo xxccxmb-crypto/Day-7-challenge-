91602477f9e52bbf6e471717e45c1bbbe70a65d7
xxccxmb-crypto <xxccxmb@gmail.com>
2025-08-20 00:18:13 +0300

    Done Day7

---
+++ script.js
@@ -0,0 +1,7 @@
+let name = prompt("Enter your name");
+let age = prompt("Enter your age");
+let city = prompt("Enter your city");
+console.log(`%cname:${name}`,"color:green;");
+console.log(`%cage:${age}`,"color:white");
+console.log(`%ccity:${city}`,"color:purple");
+alert(`Hello ${name}, your age is ${age}, your city is ${city}`);
